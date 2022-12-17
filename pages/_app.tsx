d9340f1bfc40f93b98a5705036eb83754e429ce5
sol <63882468+3hx@users.noreply.github.com>
2022-12-17 22:23:38 +0000

    added basic comingsoon page

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,11 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import "../styles/globals.css";
+import type { AppProps } from "next/app";
+import { Poppins } from "@next/font/google";
+
+const poppins = Poppins({ weight: "400", variable: "--font-poppins" });
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return <Component {...pageProps} />;
 }
 
-export default MyApp
+export default MyApp;
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,20 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './pages/**/*.{js,ts,jsx,tsx}',
-    './components/**/*.{js,ts,jsx,tsx}',
-    './app/**/*.{js,ts,jsx,tsx}',
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+    "./app/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        primary: ["var(--font-poppins)", "sans-serif"],
+      },
+      backgroundColor: {
+        primary: "#FF5A5F",
+        "primary-dark": "F43F5E",
+      },
+    },
   },
   plugins: [],
-}
+};
