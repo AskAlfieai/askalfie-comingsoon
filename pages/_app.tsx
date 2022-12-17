1ef92f211b299cbf8a1a8564b4aabe191bf52abb
sol <63882468+3hx@users.noreply.github.com>
2022-12-17 22:30:39 +0000

    added subset to font

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,7 +2,11 @@ import "../styles/globals.css";
 import type { AppProps } from "next/app";
 import { Poppins } from "@next/font/google";
 
-const poppins = Poppins({ weight: "400", variable: "--font-poppins" });
+const poppins = Poppins({
+  weight: "400",
+  variable: "--font-poppins",
+  subsets: ["latin"],
+});
 
 function MyApp({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />;
